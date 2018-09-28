17ea20a3c3074683e0e91f72ad3087666a71a367
Oleg Stepanov <oleg.v.stepanov@gmail.com>
2018-09-28 12:33:57 +0200

    Added Greeting class

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class OddsAndEvens {
     public static void main (String args[]){
-        
+        Greeting();
+    }
+
+    public static void Greeting() {
+        System.out.println("Let’s play a game called “Odds and Evens”");
+        System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.println("Hi " + name + " , which do you choose? (O)dds or (E)vens?");
+        String choice = input.next();
+        if (choice.equals("O")) {
+            System.out.println(name + " has picked odds! The computer will be evens.");
+        } else if (choice.equals("E")) {
+            System.out.println(name + " has picked evens! The computer will be odds.");
+        } else {
+            System.out.println("Please type eather O or P");
+        }
     }
 }
